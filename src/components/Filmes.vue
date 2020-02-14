6c1d96af15dd9e2637b9291dca1c2bb6ac77de5a
leoewbankinfnet <leonardo.ewbank@al.infnet.edu.br>
2020-02-14 08:12:23 -0300

    foifoi

--- src/components/Filmes.vue
+++ src/components/Filmes.vue
@@ -58,9 +58,9 @@
       tag = "article"
       class="mb-2"
       >
-        <b-card-text> {{filme.descricao}} </b-card-text>
-        <b-card-text> R${{filme.valor}} </b-card-text>
-        <a href="#" class="card-link">Clique aqui para alugar</a>
+        <b-card-text v-html='filme.descricao'>  </b-card-text>
+        <b-card-text> {{filme.valor|formatarPreco("R$")}} </b-card-text>
+         <b-button variant="outline-primary"><a href="#" class="card-link">Clique aqui para alugar</a></b-button>
       </b-card>
     </b-col>
   
@@ -77,11 +77,11 @@ export default {
   data:function (){
     return{
       filmes: 
-        [ {id:1, titulo: "Vingadores", descricao:"Um filme de heróis", valor:25, imagem:"https://cdn.telecineplay.com.br/isl/api/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27141726%27&EntityType=%27Item%27&EntityId=%273863%27&Width=1080&Height=1600&device=web_browser&subscriptions=Anonymous"},
-          {id:2, titulo: "Vingadores 2 ", descricao:"Um filme de heróis", valor:25,imagem:"https://upload.wikimedia.org/wikipedia/pt/thumb/2/22/OsVingadores2.jpg/250px-OsVingadores2.jpg"},
-          {id:3, titulo: "Pantera Negra", descricao:"Um filme de heróis", valor:25,imagem:"https://cdn.telecineplay.com.br/isl/api/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27182460%27&EntityType=%27Item%27&EntityId=%2710148%27&Width=720&Height=1080&device=web_browser&subscriptions=Anonymous"},
-          {id:4, titulo: "Capitã Marvel", descricao:"Um filme de heróis", valor:25, imagem:"https://upload.wikimedia.org/wikipedia/pt/thumb/5/59/Captain_Marvel_%282018%29.jpg/250px-Captain_Marvel_%282018%29.jpg"},
-          {id:5, titulo: "Brightburn", descricao:"Um filme de heróis", valor:25, imagem:"https://s2.glbimg.com/ymb71yQSk38YMYeoe0717NTTisk=/e.glbimg.com/og/ed/f/original/2019/05/21/brightburn_regposter_brazil.jpg"}
+        [ {id:1, titulo: "Vingadores", descricao:"Um <sub>filme</sub> de <b>heróis</b>", valor:25, imagem:"https://cdn.telecineplay.com.br/isl/api/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27141726%27&EntityType=%27Item%27&EntityId=%273863%27&Width=1080&Height=1600&device=web_browser&subscriptions=Anonymous"},
+          {id:2, titulo: "Vingadores 2 ", descricao:"Um <sup>filme</sup> de heróis", valor:25,imagem:"https://upload.wikimedia.org/wikipedia/pt/thumb/2/22/OsVingadores2.jpg/250px-OsVingadores2.jpg"},
+          {id:3, titulo: "Pantera Negra", descricao:"Um <i>filme de</i> heróis", valor:25,imagem:"https://cdn.telecineplay.com.br/isl/api/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27182460%27&EntityType=%27Item%27&EntityId=%2710148%27&Width=720&Height=1080&device=web_browser&subscriptions=Anonymous"},
+          {id:4, titulo: "Capitã Marvel", descricao:"Um <mark>filme</mark> de heróis", valor:25, imagem:"https://upload.wikimedia.org/wikipedia/pt/thumb/5/59/Captain_Marvel_%282018%29.jpg/250px-Captain_Marvel_%282018%29.jpg"},
+          {id:5, titulo: "Brightburn", descricao:"<strong>Um filme de heróis</strong>", valor:25, imagem:"https://s2.glbimg.com/ymb71yQSk38YMYeoe0717NTTisk=/e.glbimg.com/og/ed/f/original/2019/05/21/brightburn_regposter_brazil.jpg"}
 
         ]
     }
