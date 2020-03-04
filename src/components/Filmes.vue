a9a198324281913e59c67f7c05ffb3f7f7335ab2
leoewbankinfnet <56918110+leoewbankinfnet@users.noreply.github.com>
2020-03-04 09:18:56 -0300

    a

--- src/components/Filmes.vue
+++ src/components/Filmes.vue
@@ -40,7 +40,12 @@
         </span>
         <b-card-text > {{filme.descricao}} </b-card-text>
         <b-card-text> {{filme.valor|formatarPreco("R$")}} </b-card-text>
-         
+         <div class="avaliacao">
+              <span v-for="n in 5" :key="n"
+                v-bind:class="{'avaliacao-active': checarAvaliacao(n,filme)}">
+                <img src="../assets/star.jpg" height="20"/>
+              </span>
+         </div>
           <a href="#" 
           class="btn btn-primary" 
           @click="adicionarCarrinho(filme)" 
@@ -199,11 +204,11 @@ export default {
     return{
       mostrarFilmes:true,
       filmes: 
-        [ {id:1, titulo: "Vingadores", descricao:"Um filme de heróis", valor:25, imagem:"https://cdn.telecineplay.com.br/isl/api/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27141726%27&EntityType=%27Item%27&EntityId=%273863%27&Width=1080&Height=1600&device=web_browser&subscriptions=Anonymous",estoqueDisp: 3},
+        [ {id:1, titulo: "Vingadores", descricao:"Um filme de heróis", valor:25, imagem:"https://cdn.telecineplay.com.br/isl/api/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27141726%27&EntityType=%27Item%27&EntityId=%273863%27&Width=1080&Height=1600&device=web_browser&subscriptions=Anonymous",estoqueDisp: 3,avaliacao:3},
           {id:2, titulo: "Vingadores 2 ", descricao:"Um filme de heróis", valor:25,imagem:"https://upload.wikimedia.org/wikipedia/pt/thumb/2/22/OsVingadores2.jpg/250px-OsVingadores2.jpg",estoqueDisp:7},
-          {id:3, titulo: "Pantera Negra", descricao:"Um filme de heróis", valor:25,imagem:"https://cdn.telecineplay.com.br/isl/api/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27182460%27&EntityType=%27Item%27&EntityId=%2710148%27&Width=720&Height=1080&device=web_browser&subscriptions=Anonymous",estoqueDisp: 34},
-          {id:4, titulo: "Capitã Marvel", descricao:"Um filme de heróis", valor:25, imagem:"https://upload.wikimedia.org/wikipedia/pt/thumb/5/59/Captain_Marvel_%282018%29.jpg/250px-Captain_Marvel_%282018%29.jpg",estoqueDisp: 4},
-          {id:5, titulo: "Brightburn", descricao:"Um filme de heróis", valor:25, imagem:"https://s2.glbimg.com/ymb71yQSk38YMYeoe0717NTTisk=/e.glbimg.com/og/ed/f/original/2019/05/21/brightburn_regposter_brazil.jpg",estoqueDisp: 8}
+          {id:3, titulo: "Pantera Negra", descricao:"Um filme de heróis", valor:25,imagem:"https://cdn.telecineplay.com.br/isl/api/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27182460%27&EntityType=%27Item%27&EntityId=%2710148%27&Width=720&Height=1080&device=web_browser&subscriptions=Anonymous",estoqueDisp: 34,avaliacao:2},
+          {id:4, titulo: "Capitã Marvel", descricao:"Um filme de heróis", valor:25, imagem:"https://upload.wikimedia.org/wikipedia/pt/thumb/5/59/Captain_Marvel_%282018%29.jpg/250px-Captain_Marvel_%282018%29.jpg",estoqueDisp: 4,avaliacao:1},
+          {id:5, titulo: "Brightburn", descricao:"Um filme de heróis", valor:25, imagem:"https://s2.glbimg.com/ymb71yQSk38YMYeoe0717NTTisk=/e.glbimg.com/og/ed/f/original/2019/05/21/brightburn_regposter_brazil.jpg",estoqueDisp: 8,avaliacao:2}
         ],
         carrinho:[],
       pedido:{
@@ -247,6 +252,9 @@ export default {
     },
     submitFormulario(){
       alert("Pedido Finalizado");
+    },
+    checarAvaliacao(n,filme){
+      return filme.avaliacao - n >= 0;
     }
   },
   computed: {
@@ -290,6 +298,15 @@ a {
 }
 .mensagem-estoque{
   font-weight: bold;
+}
+
+span.avaliacao-active {
+  background-image:url('../assets/star-fill.jpg');
+  background-repeat: no-repeat;
+  background-position-y: 3px;
+  background-position-x:1px;
+  background-size: 20px;
+  padding:1px;
 }
 
 </style>
